67e2a4758970c1c21dae08b01185eb4544f188cb
youyuyixiu <xiaoshuyi123@163.com>
2018-06-12 19:24:42 +0800

    pycharm

--- test.py
+++ test.py
@@ -3,3 +3,19 @@
 # author: youyuyixiu
 # time: 2018/6/12
 
+# pycharm 用法
+
+
+def f():
+    pass
+
+# ctrl+d 快速复制一行
+# ctrl+y 快速删除一行
+# shift+enter 快速换行
+# ctrl+/ 注释
+# tab 缩进
+# shift+tab 反缩进
+# ctrl+f 查找
+# ctrl+r 替换
+# ctrl+加减号  折叠
+# Ctrl+shift+加减号  全局折叠
