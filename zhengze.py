49748da568993ba21fe1c5ba43a206abcddd67f7
youyuyixiu <xiaoshuyi123@163.com>
2018-06-12 17:41:20 +0800

    zhengze

---
+++ zhengze.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+import re
+
+m = re.findall('abc', 'aaaaabcccabcc')
+print(m)
+
+m = re.findall('\d', 'abc1ab2c')
+print(m)
+
+m = re.findall('\d\d\d\d', '123abc1234abc')
+print(m)
+
+m = re.findall(r'<div>(.*)</div>', '<div>hello</div>')
+print(m)
+
+m = re.findall(r'<div>(.*)</div>', '<div>hello</div><div>world</div>')
+print(m)
+
+m = re.findall(r'<div>(.*?)</div>', '<div>hello</div><div>world</div><div>hi</div>')
+print(m)
+
